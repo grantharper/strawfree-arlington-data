b47c1ad38af3d82264fab429dbbc52abc5bdc0a6
grantharper <grant.e.harper@gmail.com>
2018-10-27 09:35:26 -0400

    extract csv constants to class level

--- src/main/kotlin/org/grantharper/strawfree/StrawfreeApplication.kt
+++ src/main/kotlin/org/grantharper/strawfree/StrawfreeApplication.kt
@@ -13,19 +13,27 @@ import java.io.FileReader
 class StrawfreeApplication: CommandLineRunner {
 
     val logger: Logger = LoggerFactory.getLogger(this.javaClass)
-
+    val inputFilename = "input.csv"
+    val nameHeader = "Name"
+    val starCountHeader = "Star Count"
 
     override fun run(vararg args: String?) {
         logger.info("Kotlin command line running")
-        val inputFile = FileReader("input.csv")
-        val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(inputFile)
-        for (record in records) {
-            val name = record.get("Name")
-            val starCount = record.get("Star Count")
-            logger.info("name=${name}, starCount=${starCount}")
-        }
+        processCsv()
 
     }
+
+    private fun processCsv() {
+
+        val inputFile = FileReader(inputFilename)
+        val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(inputFile)
+        for (record in records) {
+            val name = record.get(nameHeader)
+            val starCount = record.get(starCountHeader)
+            logger.info("name=${name}, starCount=${starCount}")
+        }
+    }
+
 }
 
 fun main(args: Array<String>) {
